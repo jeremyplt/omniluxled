c2b6870e5f4ce6fb6ed876bf782df7e7f2a13eac
Jeremy <89654634+jeremyplt@users.noreply.github.com>
2024-03-12 22:57:12 +0800

    fixing the pre-order buttonl label according to country

--- assets/location-handler.js
+++ assets/location-handler.js
@@ -1,6 +1,6 @@
 class LocationHandler {
   constructor() {
-    this.PDPCartBtnElements = document.querySelectorAll('.data-submit-btn');
+    this.PDPCartBtnElements = document.querySelectorAll(".data-submit-btn");
   }
 
   onPageLoad() {
@@ -8,39 +8,39 @@ class LocationHandler {
   }
 
   async locationHandler() {
-    const country = localStorage.getItem('country') || 'United States';
+    const country = localStorage.getItem("country") || "United States";
 
     const prefixMap = {
-      'United States': 'us',
-      'United Kingdon': 'uk',
-      'Canada': 'ca',
-      'Australia': 'au',
-      'New Zealand': 'nz',
-    }
+      "United States": "us",
+      "United Kingdon": "uk",
+      Canada: "ca",
+      Australia: "au",
+      "New Zealand": "nz",
+    };
 
-    const prefix = prefixMap[country] || 'eu'
+    const prefix = prefixMap[country] || "eu";
 
-    console.log(prefix)
+    console.log(prefix);
 
-    this.pdp(prefix)
-    this.cart(prefix)
+    this.pdp(prefix);
+    this.cart(prefix);
   }
 
   cart(prefix) {
     const payIconTag = document.querySelector(
-      '.shopify-installments__content-image'
+      ".shopify-installments__content-image"
     );
-    if (prefix == 'us') {
+    if (prefix == "us") {
       payIconTag.setAttribute(
-        'src',
-        'https://cdn.shopify.com/s/files/1/0482/6736/2466/files/ShopPay_-_Color.svg?v=1697732176'
+        "src",
+        "https://cdn.shopify.com/s/files/1/0482/6736/2466/files/ShopPay_-_Color.svg?v=1697732176"
       );
     } else {
       payIconTag.setAttribute(
-        'src',
-        'https://cdn.shopify.com/s/files/1/0482/6736/2466/files/Afterpay_Mint_Logo.jpg?v=1687306677'
+        "src",
+        "https://cdn.shopify.com/s/files/1/0482/6736/2466/files/Afterpay_Mint_Logo.jpg?v=1687306677"
       );
-      payIconTag.setAttribute('style', 'display:inline;width:90px;');
+      payIconTag.setAttribute("style", "display:inline;width:90px;");
     }
   }
 
@@ -54,21 +54,21 @@ class LocationHandler {
       this.PDPCartBtnElements.forEach((btn) => {
         const comingsoonStatus = btn.dataset?.[comingsoonStatusKey];
 
-        if (comingsoonStatus === 'true') {
+        if (comingsoonStatus === "true") {
           btn.innerHTML = '<span class="add-text">Coming Soon</span>';
-          btn.classList.add('disabled');
+          btn.classList.add("disabled");
         } else {
           const preorderStatus = btn.dataset?.[preorderStatusKey];
           const preorderMessage = btn.dataset?.[preorderMessageKey];
 
-          if (preorderStatus === 'true') {
-            btn.innerHTML = '<span class="add-text">Pre-order</span>';
-          }
+          // if (preorderStatus === 'true') {
+          //   btn.innerHTML = '<span class="add-text">Pre-order</span>';
+          // }
 
           const msgEle = btn
-            .closest('[data-product-form]')
-            .querySelector('[data-product-preorder-message]');
-          if (preorderStatus === 'true' && preorderMessage !== '' && msgEle) {
+            .closest("[data-product-form]")
+            .querySelector("[data-product-preorder-message]");
+          if (preorderStatus === "true" && preorderMessage !== "" && msgEle) {
             msgEle.innerHTML = `
                       <div class="align-items-center mt-2 mb-lg-3 mb-2 py-1 formatted-list fw-normal">
                          <ul>${preorderMessage}</ul>
@@ -82,4 +82,4 @@ class LocationHandler {
 }
 
 const locationHandler = new LocationHandler();
-window.addEventListener('DOMContentLoaded', () => locationHandler.onPageLoad());
+window.addEventListener("DOMContentLoaded", () => locationHandler.onPageLoad());
