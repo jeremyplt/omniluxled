d0ec28651a8439a489ef943533898342d2a2a0b2
Jeremy <89654634+jeremyplt@users.noreply.github.com>
2024-04-03 12:37:58 +0800

    chore: theme sync

--- assets/hoox-slider-ugc.js
+++ assets/hoox-slider-ugc.js
@@ -1,16 +1,21 @@
 var hooxSliderReviews = document.querySelector(".hoox-ugc-videos__items");
 
 if (hooxSliderReviews) {
+  
+
+function isMobileDevice() {
+  return window.innerWidth <= 767; // Adjust the width as per your mobile breakpoint
+}
+
+// Example usage
+if (isMobileDevice()) {
+console.log("on mobile");
+  // Code for mobile devices
+
   var slider = new Swiper(hooxSliderReviews, {
   loop: true,
-  slidesPerView: 4,
-  centeredSlides: true,
-  breakpoints: {
-    767: {
-      slidesPerView: 1,
-      slidesPerGroup: 1,
-    },
-  },
+  slidesPerView: 1, // Show 4 slides at once
+    // 
   pagination: {
     el: '.hoox-swiper-center__pagination',
     clickable: true,
@@ -18,13 +23,38 @@ if (hooxSliderReviews) {
   navigation: {
     nextEl: ".slider-centered-next",
     prevEl: ".slider-centered-prev",
-  },
-  on: {
-    slideChange: function () {
-      if (this.isEnd) {
-        this.slideTo(0); // Go to the first slide when reaching the end
-      }
-    }
   }
 });
+} else {
+  console.log("on desktop");
+  var slider = new Swiper(hooxSliderReviews, {
+  loop: true,
+  slidesPerView: 4, // Show 4 slides at once
+  spaceBetween: 20,
+   breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+  },
+     
+    // 
+  pagination: {
+    el: '.hoox-swiper-center__pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".slider-centered-next",
+    prevEl: ".slider-centered-prev",
+  }
+});
+}
 }
